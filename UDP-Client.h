e07450cbfe98fe4bbd3fdb164b05ce00fb9b427e
Elijah Madden <elijah.m.madden@gmail.com>
2018-02-16 17:24:04 -0700

    Header files for client and server

---
+++ UDP-Client.h
@@ -0,0 +1,15 @@
+/*
+ * UDP-Client.h
+ *
+ *  Created on: Feb 16, 2018
+ *      Author: Elijah
+ */
+
+#ifndef UDP_CLIENT_H_
+#define UDP_CLIENT_H_
+
+
+#endif /* UDP_CLIENT_H_ */
+
+void DieWithError(const char* errorMessage);
+int start_client(int argc, char** argv);
---
+++ UDP-Server.h
@@ -0,0 +1,15 @@
+/*
+ * UDP-Server.h
+ *
+ *  Created on: Feb 16, 2018
+ *      Author: Elijah
+ */
+
+#ifndef UDP_SERVER_H_
+#define UDP_SERVER_H_
+
+
+#endif /* UDP_SERVER_H_ */
+
+void DieWithError(const char *errorMessage);
+int start_server(int argc, char** argv);
